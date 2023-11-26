098ee246c6ff09f635a169fc0b86a1aa9674fce2
Rafael Marques <marquesrf@protonmail.com>
2023-11-26 00:05:53 -0300

    safe-bkp

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 class ContatoController extends Controller
 {
     public function principal() {
-        var_dump($_POST);
         $titulo = 'Contato';
         return view('site.contato', compact('titulo'));
     }
